797052aee0c4c1bcf742071e4f8c3f9005d6f77a
Shuntaro Ohno <sohno@ushitora.net>
2025-07-01 09:52:27 +0900

    add favicon

--- dist/exif-edit-354d1234ebaf38e8.js
+++ dist/exif-edit-354d1234ebaf38e8.js
@@ -233,7 +233,7 @@ function debugString(val) {
     return className;
 }
 function __wbg_adapter_48(arg0, arg1) {
-    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h43160516a6512c54(arg0, arg1);
+    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0b93de7e072ba652(arg0, arg1);
 }
 
 function __wbg_adapter_51(arg0, arg1, arg2) {
